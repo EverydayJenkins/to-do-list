20afd70653d7e7ced9eadeebc207953d6831ab36
Walter Sobczak <josephykrakowsky@gmail.com>
2022-07-12 19:25:02 +0200

    initial commit

---
+++ ToDoList/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string Status { get; set; }
+        public DateTime? DateFinished { get; set; }
+
+    }
+}
